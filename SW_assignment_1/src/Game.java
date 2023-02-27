8ce7c25a43bd90b103dbcc8e047e65216f9c9ecb
Abdelrahman Mohamed <11410120210229@stud.cu.edu.eg>
2023-02-27 19:42:43 +0200

    Update Game.java

--- SW_assignment_1/src/Game.java
+++ SW_assignment_1/src/Game.java
@@ -1,6 +1,6 @@
 public abstract class Game {
-    protected XO_Board board;
-    protected  XO_Player[] players;
+    protected Board board;
+    protected  Player[] players;
     protected int turn;
     protected _2DIndex move;
     abstract void play_game();
